369f7a10f4fbfaa45c41093cb5c1ac6f9a5769cb
Arseniy Olevskiy <arsidian@gmail.com>
2023-10-24 16:00:07 +0400

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,6 +10,8 @@ License: GPLv2 or later
 Text Domain: ci_hours_and_minutes_calculator
 */
 
+if (!defined('ABSPATH')) exit;
+
 if (!function_exists('add_shortcode')) return "No direct call for Hours and Minutes Calculator by Calculator.iO";
 
 function display_ci_hours_and_minutes_calculator(){
